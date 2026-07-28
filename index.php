5aadc37dc161294e3743dec4528dcd4154ea13a6
aliikram12 <info.aliikram57@gmail.com>
2026-07-28 22:51:57 +0500

    Conditions

--- index.php
+++ index.php
@@ -99,5 +99,34 @@
             $name = $_GET['user'] ?? "Guest";  // null coalescing
             echo $name;
     ?>
+
+
+
+
+    <?php
+            $grade = 85;
+
+            if ($grade >= 90) {
+                echo "A";
+            } elseif ($grade >= 80) {
+                echo "B";
+            } elseif ($grade >= 70) {
+                echo "C";
+            } else {
+                echo "D or F";
+            }
+
+            $day = "Monday";
+            switch ($day) {
+                case "Monday":
+                    echo "Start of week";
+                    break;
+                case "Friday":
+                    echo "Weekend soon";
+                    break;
+                default:
+                    echo "Midweek";
+            }
+?>
 </body>
 </html>
